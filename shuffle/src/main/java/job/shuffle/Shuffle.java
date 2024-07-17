ece5e471d1c7bf327b1e3f39e25990bce65a958b
ghhamo <93213332+ghhamo@users.noreply.github.com>
2024-07-17 19:01:29 +0400

    add shuffle

---
+++ shuffle/src/main/java/job/shuffle/Shuffle.java
@@ -0,0 +1,18 @@
+package job.shuffle;
+
+import java.util.Random;
+
+public class Shuffle<V extends Comparable<V>> {
+
+    public V[] shuffling(V[] collection) {
+        Random random = new Random();
+        for (int i = 0; i < collection.length; i++) {
+            V left = collection[i];
+            int rightIndex = random.nextInt(i + 1, collection.length);
+            collection[i] = collection[rightIndex];
+            collection[rightIndex] = left;
+
+        }
+        return collection;
+    }
+}
